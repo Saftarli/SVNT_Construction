59784e070e283ab0a2c84db9c62c08b8bedc2e55
saftarli <tofiqsefterli@gmail.com>
2024-10-03 07:13:00 +0400

    2 ci variant

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder='templates',static_folder='static', static_url_path='/static')
@@ -8,7 +10,17 @@ db = SQLAlchemy(app)
 
 class ContactMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.Text, nullable=False)
 
+    def __init__(self, name, email, message):
+        self.name = name
+        self.email = email
+        self.message = message
+
+with app.app_context():
+    db.create_all()
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -16,9 +28,19 @@ def home():
 @app.route('/submit-form', methods=['POST'])
 def submit_form():
     name = request.form.get('name')
-    mail = request.form.get('mail')
+    email = request.form.get('email')
     message = request.form.get('message')
-    return redirect(url_for('home'))
+
+    new_message = ContactMessage(name=name, email=email, message=message)
+    db.session.add(new_message)
+    db.session.commit()
+
+    return redirect(url_for('home '))
+@app.route('/thanks')
+def thanks():
+    return "teesekkur"
+
+
 
 if __name__ == '__main__':
     app.run()
